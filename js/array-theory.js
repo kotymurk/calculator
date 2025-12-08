5a1bbb0b7da97a4d55179379a933fe2c1e0dd946
kotymurk <kotymurk@vk.com>
2025-12-08 23:20:48 +0200

    array

--- js/array-theory.js
+++ js/array-theory.js
@@ -5,5 +5,33 @@ const names = ['Елена', 'Игорь', 'Екатерина', 'Владиле
 
 //const name = names.shift();
 // const name = names.pop();
+// console.log('Names: ', names, name);
 
-console.log('Names: ', names, name);
+// console.log(names.reverse());
+// const reversed = names.toReversed();
+// const letters = ['e', 'c', 'd', 'b', 'a'];
+// console.log(letters.toSorted());
+// console.log(letters);
+
+// console.log(names.toSpliced(2, 1));
+// console.log(names);
+
+//const greatWoman = 'Елена';
+//const index1 = names.indexOf(greatWoman);
+// console.log(index1);
+//const newNames = names.with(index1, 'Елена Великая');
+// console.log(names);
+// console.log(newNames);
+
+// const capitalNames = names.map(function (name, index1) {
+//   if (index1 === 0) {
+//     return 'Елена Великая';
+//   }
+//   return name;
+// });
+
+// console.log(capitalNames);
+
+//console.log(names.includes('Игорь'));
+
+// console.log(names.indexOf('Игорь') !== -1);
